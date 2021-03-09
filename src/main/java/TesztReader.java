99a5d271e5064e5fab9b3e62d951413f7b1f0b7c
szendre1 <endre.szarka@gmail.com>
2021-03-09 13:28:28 +0100

    Teszt

--- src/main/java/TesztReader.java
+++ src/main/java/TesztReader.java
@@ -11,7 +11,7 @@ public class TesztReader {
 
         while ((line = reader.readLine()) != null) {
             content.append(line);
-            content.append(System.lineSeparator() ); //xxxxxxxxxxxxxxxxx
+            content.append(System.lineSeparator() ); //xxxxxxxxxxxxxxxx   x
         }
 
         System.out.println(content);
